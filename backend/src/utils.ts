c80ee1ba58e3ae41b8efac816468828516d8e08a
Loïc M <loic.mura@student-cs.fr>
2023-04-04 14:48:30 +0200

    Remove old examples

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,7 +1,3 @@
-export type Response<T> =
-  | { success: true; payload: T }
-  | { success: false; errors: string[] };
-
 export type Result<T, E = string> =
   | { success: true; payload: T }
   | { success: false; error: E };
